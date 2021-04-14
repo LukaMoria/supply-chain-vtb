f5bf5f47946a633b686d97fa8102b27b2f8d2b08
nrostovshchikov <nrostovshchikov@ozon.ru>
2021-04-14 22:08:57 +0300

    prepare layout for pages

---
+++ supply-chain/src/components/layout/leftMenu.vue
@@ -0,0 +1,49 @@
+<template>
+    <q-drawer
+      :value="leftMenuDrawer"
+      @input="(value) => $emit('input', value)"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header class="label">Меню</q-item-label>
+        <q-item v-for="(item,index) in links" :key="index" :to="item.href" clickable>
+          <q-item-section avatar>
+            <q-icon :name="item.icon" />
+          </q-item-section>
+          <q-item-section class="label">
+            <q-item-label>{{ item.name }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+</template>
+
+<script>
+export default {
+  name:'Menu',
+  props: {
+    leftMenuDrawer: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      links: [
+        { href: '/profile', name: 'Мой профиль', icon: 'face' },
+        { href: '/offers', name: 'Предложения', icon: 'shop'},
+        { href: '/purchases', name: 'Мои покупки', icon: 'cases'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .label {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+</style>
---
+++ supply-chain/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Home qweqwekqwekqw asdnakndklasndklasndklansdlkasndlkans</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {},
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
