dc4d2bb3c18dd2b21c72b1ab0ab593691041a313
nrostovshchikov <nrostovshchikov@ozon.ru>
2021-04-14 09:58:04 +0300

    start web3js

--- supply-chain/src/App.vue
+++ supply-chain/src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
-    Hello
+    <button @click="createOffer">createOffer</button>
   </div>
 </template>
 
 <script>
+import instance from './contracts/marketplaceBoxInstance'
 
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  async mounted() {
+    console.log(instance)
+    const res = await instance.methods.returnAllOffers().call({from: '0x06221c24fBa452c2a2716F9Ec705fd001536296a'})
+    console.log(res)
+  },
+  methods:{
+    async createOffer() {
+      const res = await instance.methods.createOffer('test', 250, 'qweqwe', 150).send({from: '0x06221c24fBa452c2a2716F9Ec705fd001536296a'})
+      console.log(res)
+      const res2 = await instance.methods.returnAllOffers().send({from: '0x06221c24fBa452c2a2716F9Ec705fd001536296a'})
+      console.log(res2)
+    }
+  }
 }
 </script>
